68d7039a61a9d340879a5dde065b459bf3ab47b8
SuheiIwamoto <shuhei.iwamoto.work@gmail.com>
2019-09-07 17:23:49 +0900

    add RawImage static functions

--- Scripts/Runtime/UnityEngine/ExRawImage.cs
+++ Scripts/Runtime/UnityEngine/ExRawImage.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine.UI;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace UnityExtensions
 {
@@ -21,27 +22,57 @@ namespace UnityExtensions
         }
 
         public static void SetUvRectX(this RawImage self, float value) {
-            var uvRect = self.uvRect;
+            Rect uvRect = self.uvRect;
             uvRect.x = value;
             self.uvRect = uvRect;
         }
 
         public static void SetUvRectY(this RawImage self, float value) {
-            var uvRect = self.uvRect;
+            Rect uvRect = self.uvRect;
             uvRect.y = value;
             self.uvRect = uvRect;
         }
 
         public static void SetUvRectWidth(this RawImage self, float value) {
-            var uvRect = self.uvRect;
+            Rect uvRect = self.uvRect;
             uvRect.width = value;
             self.uvRect = uvRect;
         }
 
         public static void SetUvRectHeight(this RawImage self, float value) {
-            var uvRect = self.uvRect;
+            Rect uvRect = self.uvRect;
             uvRect.height = value;
             self.uvRect = uvRect;
+        }
+        
+        /// <summary>
+        /// アスペクト比に合わせてRawImageのサイズを修正する
+        /// 現在のUIサイズが基準となる
+        /// </summary>
+        /// <see cref="https://github.com/nkjzm/FixAspectSample/tree/master"/>
+        public static void FixAspect(this RawImage self)
+        {
+            self.FixAspect(self.rectTransform.rect.size);
+        }
+        
+        /// <summary>
+        /// アスペクト比に合わせてRawImageのサイズを修正する
+        /// </summary>
+        /// <param name="originalSize">基準となるUIサイズ</param>
+        /// <see cref="https://github.com/nkjzm/FixAspectSample/tree/master"/>
+        public static void FixAspect(this RawImage self, Vector3 originalSize)
+        {
+            Vector2 textureSize = new Vector2(self.texture.width, self.texture.height);
+
+            float heightScale = originalSize.y / textureSize.y;
+            float widthScale = originalSize.x / textureSize.x;
+            Vector2 rectSize = textureSize * Mathf.Min(heightScale, widthScale);
+
+            Vector2 anchorDiff = self.rectTransform.anchorMax - self.rectTransform.anchorMin;
+            Vector2 parentSize = (self.transform.parent as RectTransform).rect.size;
+            Vector2 anchorSize = parentSize * anchorDiff;
+
+            self.rectTransform.sizeDelta = rectSize - anchorSize;
         }
     }
 }
